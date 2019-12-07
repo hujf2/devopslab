f3fad733414acfec00c7bfed238def21ae247180
hujf2 <419644560@qq.com>
2019-12-07 14:35:12 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,10 +1,10 @@
 export GITACCOUNT=hujf2
 
 export MYCLUSTER=tektonknativebeijing62
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing62/kube-config-tok04-tektonknativebeijing62.yml
 
 export NAMESPACE=tektonknativebeijing62
 export EMAIL=419644560@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing62-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
 
 
